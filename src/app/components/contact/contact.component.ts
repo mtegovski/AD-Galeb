e665db71347bbcf13f4530ae7ef7a78b997afaef
Mihail Tegovski <mihail.tegovski@netcetera.com>
2026-07-29 17:47:12 +0200

    - add spinner for map pin

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
-
+  isMapLoading = signal(true);
 }
